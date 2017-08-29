d8f1189c40377600b158bc3cab13f0ca3c0c441c
Mara Timberlake <maratimberlake@msn.com>
2017-08-29 16:57:07 -0700

    add wordfiner game

---
+++ js/wordfindergame.js
@@ -0,0 +1,187 @@
+(function (document, $, wordfind) {
+
+
+  var WordFindGame = function() {
+
+    var wordList;
+    var drawPuzzle = function (el, puzzle) {
+
+      var output = '';
+      //Each row of the puzzle...
+      for (var i = 0, height = puzzle.length; i < height; i++) {
+        var row = puzzle[i];
+        output += '<div>';
+        for (var j = 0, width = row.length; j < width; j++) {
+            output += '<button class="puzzleSquare" x="' + j + '" y="' + i + '">';
+            output += row[j] || '&nbsp;';
+            output += '</button>';
+        }
+        // close our div that represents a row
+        output += '</div>';
+      }
+
+      $(el).html(output);
+    };
+
+    var drawWords = function (el, words) {
+      var output = '<ul>';
+      for (var i = 0, len = words.length; i < len; i++) {
+        var word = words[i];
+        output += '<li class="word ' + word + '">' + word;
+      }
+      output += '</ul>';
+      $(el).html(output);
+    };
+
+
+    // Game state
+    var startSquare, selectedSquares = [], curOrientation, curWord = '';
+
+    var startTurn = function () {
+      $(this).addClass('selected');
+      startSquare = this;
+      selectedSquares.push(this);
+      curWord = $(this).text();
+    };
+
+    var select = function (target) {
+      if (!startSquare) {
+        return;
+      }
+      var lastSquare = selectedSquares[selectedSquares.length-1];
+      if (lastSquare == target) {
+        return;
+      }
+
+      var backTo;
+      for (var i = 0, len = selectedSquares.length; i < len; i++) {
+        if (selectedSquares[i] == target) {
+          backTo = i+1;
+          break;
+        }
+      }
+
+      while (backTo < selectedSquares.length) {
+        $(selectedSquares[selectedSquares.length-1]).removeClass('selected');
+        selectedSquares.splice(backTo,1);
+        curWord = curWord.substr(0, curWord.length-1);
+      }
+
+      // see if the move is along the same orientation as the last move
+      var orientation = calcOrientation(
+          $(lastSquare).attr('x')-0,
+          $(lastSquare).attr('y')-0,
+          $(target).attr('x')-0,
+          $(target).attr('y')-0
+          );
+
+      // finally, if there was no previous orientation or this move is along
+      // the same orientation as the last move then play the move
+      if (!curOrientation || curOrientation === orientation) {
+        curOrientation = orientation;
+        playTurn(target);
+      }
+
+    };
+
+    var touchMove = function(e) {
+      var xPos = e.originalEvent.touches[0].pageX;
+      var yPos = e.originalEvent.touches[0].pageY;
+      var targetElement = document.elementFromPoint(xPos, yPos);
+      select(targetElement)
+    };
+
+    var mouseMove = function() {
+      select(this);
+    };
+
+    var playTurn = function (square) {
+
+      // make sure we are still forming a valid word
+      for (var i = 0, len = wordList.length; i < len; i++) {
+        if (wordList[i].indexOf(curWord + $(square).text()) === 0) {
+          $(square).addClass('selected');
+          selectedSquares.push(square);
+          curWord += $(square).text();
+          break;
+        }
+      }
+    };
+
+    var endTurn = function () {
+
+      // see if we formed a valid word
+      for (var i = 0, len = wordList.length; i < len; i++) {
+
+        if (wordList[i] === curWord) {
+          $('.selected').addClass('found');
+          wordList.splice(i,1);
+          $('.' + curWord).addClass('wordFound');
+        }
+
+        if (wordList.length === 0) {
+          $('.puzzleSquare').addClass('complete');
+          alert("You have completed the Word Finder!");
+          $("#helNextDiv").show();
+        }
+      }
+
+      // reset the turn
+      $('.selected').removeClass('selected');
+      startSquare = null;
+      selectedSquares = [];
+      curWord = '';
+      curOrientation = null;
+    };
+
+    var calcOrientation = function (x1, y1, x2, y2) {
+
+      for (var orientation in wordfind.orientations) {
+        var nextFn = wordfind.orientations[orientation];
+        var nextPos = nextFn(x1, y1, 1);
+
+        if (nextPos.x === x2 && nextPos.y === y2) {
+          return orientation;
+        }
+      }
+
+      return null;
+    };
+
+    return {
+
+      create: function(words, puzzleEl, wordsEl, options) {
+
+        wordList = words.slice(0).sort();
+
+        var puzzle = wordfind.newPuzzle(words, options);
+
+        // draw out all of the words
+        drawPuzzle(puzzleEl, puzzle);
+        drawWords(wordsEl, wordList);
+
+        // attach events to the buttons
+        // optimistically add events for windows 8 touch
+        if (window.navigator.msPointerEnabled) {
+          $('.puzzleSquare').on('MSPointerDown', startTurn);
+          $('.puzzleSquare').on('MSPointerOver', select);
+          $('.puzzleSquare').on('MSPointerUp', endTurn);
+        }
+        else {
+          $('.puzzleSquare').mousedown(startTurn);
+          $('.puzzleSquare').mouseenter(mouseMove);
+          $('.puzzleSquare').mouseup(endTurn);
+          $('.puzzleSquare').on("touchstart", startTurn);
+          $('.puzzleSquare').on("touchmove", touchMove);
+          $('.puzzleSquare').on("touchend", endTurn);
+        }
+
+        return puzzle;
+      },
+
+    };
+  };
+
+  window.wordfindgame = WordFindGame();
+
+}(document, jQuery, wordfind));
