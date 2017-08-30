847bc0a0994c1bc6b573026c933a8eb87a68d517
Mara Timberlake <maratimberlake@msn.com>
2017-08-30 14:00:35 -0700

    change links between pages and update wordfinder game

--- js/wordfindergame.js
+++ js/wordfindergame.js
@@ -1,17 +1,49 @@
+/**
+* Wordfind.js 0.0.1
+* (c) 2012 Bill, BunKat LLC.
+* Wordfind is freely distributable under the MIT license.
+* For all details and documentation:
+*     http://github.com/bunkat/wordfind
+*/
+
 (function (document, $, wordfind) {
 
+  'use strict';
 
+  /**
+  * An example game using the puzzles created from wordfind.js. Click and drag
+  * to highlight words.
+  *
+  * WordFindGame requires wordfind.js and jQuery.
+  */
+
+  /**
+  * Initializes the WordFindGame object.
+  *
+  * @api private
+  */
   var WordFindGame = function() {
 
+    // List of words for this game
     var wordList;
+
+    /**
+    * Draws the puzzle by inserting rows of buttons into el.
+    *
+    * @param {String} el: The jQuery element to write the puzzle to
+    * @param {[[String]]} puzzle: The puzzle to draw
+    */
     var drawPuzzle = function (el, puzzle) {
 
       var output = '';
-      //Each row of the puzzle...
+      // for each row in the puzzle
       for (var i = 0, height = puzzle.length; i < height; i++) {
+        // append a div to represent a row in the puzzle
         var row = puzzle[i];
         output += '<div>';
+        // for each element in that row
         for (var j = 0, width = row.length; j < width; j++) {
+            // append our button with the appropriate class
             output += '<button class="puzzleSquare" x="' + j + '" y="' + i + '">';
             output += row[j] || '&nbsp;';
             output += '</button>';
@@ -23,20 +55,41 @@
       $(el).html(output);
     };
 
+    /**
+    * Draws the words by inserting an unordered list into el.
+    *
+    * @param {String} el: The jQuery element to write the words to
+    * @param {[String]} words: The words to draw
+    */
     var drawWords = function (el, words) {
+
       var output = '<ul>';
       for (var i = 0, len = words.length; i < len; i++) {
         var word = words[i];
         output += '<li class="word ' + word + '">' + word;
       }
       output += '</ul>';
+
       $(el).html(output);
     };
 
 
+    /**
+    * Game play events.
+    *
+    * The following events handle the turns, word selection, word finding, and
+    * game end.
+    *
+    */
+
     // Game state
     var startSquare, selectedSquares = [], curOrientation, curWord = '';
 
+    /**
+    * Event that handles mouse down on a new square. Initializes the game state
+    * to the letter that was selected.
+    *
+    */
     var startTurn = function () {
       $(this).addClass('selected');
       startSquare = this;
@@ -44,15 +97,28 @@
       curWord = $(this).text();
     };
 
+
+
+    /**
+    * Event that handles mouse over on a new square. Ensures that the new square
+    * is adjacent to the previous square and the new square is along the path
+    * of an actual word.
+    *
+    */
     var select = function (target) {
+      // if the user hasn't started a word yet, just return
       if (!startSquare) {
         return;
       }
+
+      // if the new square is actually the previous square, just return
       var lastSquare = selectedSquares[selectedSquares.length-1];
       if (lastSquare == target) {
         return;
       }
 
+      // see if the user backed up and correct the selectedSquares state if
+      // they did
       var backTo;
       for (var i = 0, len = selectedSquares.length; i < len; i++) {
         if (selectedSquares[i] == target) {
@@ -67,6 +133,26 @@
         curWord = curWord.substr(0, curWord.length-1);
       }
 
+
+      // see if this is just a new orientation from the first square
+      // this is needed to make selecting diagonal words easier
+      var newOrientation = calcOrientation(
+          $(startSquare).attr('x')-0,
+          $(startSquare).attr('y')-0,
+          $(target).attr('x')-0,
+          $(target).attr('y')-0
+          );
+
+      if (newOrientation) {
+        selectedSquares = [startSquare];
+        curWord = $(startSquare).text();
+        if (lastSquare !== startSquare) {
+          $(lastSquare).removeClass('selected');
+          lastSquare = startSquare;
+        }
+        curOrientation = newOrientation;
+      }
+
       // see if the move is along the same orientation as the last move
       var orientation = calcOrientation(
           $(lastSquare).attr('x')-0,
@@ -74,6 +160,12 @@
           $(target).attr('x')-0,
           $(target).attr('y')-0
           );
+
+      // if the new square isn't along a valid orientation, just ignore it.
+      // this makes selecting diagonal words less frustrating
+      if (!orientation) {
+        return;
+      }
 
       // finally, if there was no previous orientation or this move is along
       // the same orientation as the last move then play the move
@@ -95,6 +187,11 @@
       select(this);
     };
 
+    /**
+    * Updates the game state when the previous selection was valid.
+    *
+    * @param {el} square: The jQuery element that was played
+    */
     var playTurn = function (square) {
 
       // make sure we are still forming a valid word
@@ -108,6 +205,12 @@
       }
     };
 
+    /**
+    * Event that handles mouse up on a square. Checks to see if a valid word
+    * was created and updates the class of the letters and word if it was. Then
+    * resets the game state to start a new word.
+    *
+    */
     var endTurn = function () {
 
       // see if we formed a valid word
@@ -121,8 +224,8 @@
 
         if (wordList.length === 0) {
           $('.puzzleSquare').addClass('complete');
-          alert("You have completed the Word Finder!");
-          $("#helNextDiv").show();
+          alert("You have completed the Word Search!");
+          $("#jotunheimNextDiv").show();
         }
       }
 
@@ -134,6 +237,15 @@
       curOrientation = null;
     };
 
+    /**
+    * Given two points, ensure that they are adjacent and determine what
+    * orientation the second point is relative to the first
+    *
+    * @param {int} x1: The x coordinate of the first point
+    * @param {int} y1: The y coordinate of the first point
+    * @param {int} x2: The x coordinate of the second point
+    * @param {int} y2: The y coordinate of the second point
+    */
     var calcOrientation = function (x1, y1, x2, y2) {
 
       for (var orientation in wordfind.orientations) {
@@ -150,6 +262,16 @@
 
     return {
 
+      /**
+      * Creates a new word find game and draws the board and words.
+      *
+      * Returns the puzzle that was created.
+      *
+      * @param {[String]} words: The words to add to the puzzle
+      * @param {String} puzzleEl: Selector to use when inserting the puzzle
+      * @param {String} wordsEl: Selector to use when inserting the word list
+      * @param {Options} options: WordFind options to use when creating the puzzle
+      */
       create: function(words, puzzleEl, wordsEl, options) {
 
         wordList = words.slice(0).sort();
@@ -179,9 +301,41 @@
         return puzzle;
       },
 
+      /**
+      * Solves an existing puzzle.
+      *
+      * @param {[[String]]} puzzle: The puzzle to solve
+      * @param {[String]} words: The words to solve for
+      */
+      solve: function(puzzle, words) {
+
+        var solution = wordfind.solve(puzzle, words).found;
+
+        for( var i = 0, len = solution.length; i < len; i++) {
+          var word = solution[i].word,
+              orientation = solution[i].orientation,
+              x = solution[i].x,
+              y = solution[i].y,
+              next = wordfind.orientations[orientation];
+
+          if (!$('.' + word).hasClass('wordFound')) {
+            for (var j = 0, size = word.length; j < size; j++) {
+              var nextPos = next(x, y, j);
+              $('[x="' + nextPos.x + '"][y="' + nextPos.y + '"]').addClass('solved');
+            }
+
+            $('.' + word).addClass('wordFound');
+          }
+        }
+
+      }
     };
   };
 
+
+  /**
+  * Allow game to be used within the browser
+  */
   window.wordfindgame = WordFindGame();
 
 }(document, jQuery, wordfind));
