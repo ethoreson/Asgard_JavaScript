28651cd67d64ef69edcc1fc3a816d70e44de2105
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-30 11:50:47 -0700

    save point, testing board object

--- js/midgard.js
+++ js/midgard.js
@@ -24,6 +24,10 @@ Game.prototype.populateBlanks = function(word) {
   return this.correctlyGuessed;
 }
 
+Game.prototype.replaceAt = function(index, replacement, string) {
+  return string.substr(0, index) + replacement + string.substr(index + replacement.length);
+}
+
 Game.prototype.takeAGuess = function(guess, answer) {
   var result = answer.includes(guess);
   if (result === true) {
@@ -35,17 +39,18 @@ Game.prototype.takeAGuess = function(guess, answer) {
   return result;
 };
 
-Game.prototype.displayResult = function(letter, array) {
+Game.prototype.displayResult = function(letter, array, answer, board) {
   var changeBlankHere = [];
+  console.log("answer: " + answer)
   console.log("array of word: " + array);
-  console.log("should be length of word: " + array.length); //
-  for (var i=0; i<array.length; i++) {
-    console.log(array.indexOf(i)); // 'spell out word';
-    if (letter = array.indexOf(i)) { // if 'x' is this letter
-      changeBlankHere.push(i); //push index into array
-      console.log("should be 0: " + changeBlankHere); //should be 0, or 0, 3 if elves
-      this.correctlyGuessed.replace(letter, i);
-      console.log(correctlyGuessed);
+  console.log("should be length of word: " + array.length);
+  debugger; //
+  for (var i=0; i<answer.length; i++) { // for 5 times do:
+    console.log(i);
+    console.log(answer.indexOf(letter));
+    if (i === answer.indexOf(letter)) { // if 'w' is w
+      var newDisplay = this.replaceAt(i, letter, board);
+      console.log(newDisplay);
     }
   }
 };
@@ -67,34 +72,31 @@ Game.prototype.checkForEndGame = function(word) {
 $(document).ready(function() {
   var game = new Game(10, false, []);
   var answer = game.selectWord(wordArray);
-  var board = game.populateBlanks(answer); //board = _____
+  var board = game.populateBlanks(answer);
+  $(".displayBoard").append(board);
+
   console.log("ANSWER: " + answer);
   var answerArray = game.generateArray(answer);
   $("#letterSubmit").submit(function(event) {
     event.preventDefault();
     var letter = $("#letter").val();
     var print = game.takeAGuess(letter, answer);
-    if (print = true) {
-      game.displayResult(letter, answerArray);
-    }
-
-    var final = game.checkForEndGame(answer);
-    if (final = true && game.guessesLeft === 0) {
-      alert("You lost the game! Lose a life and continue");
-      $("#alfheimNextDiv").show();
-    } else if (final = true) {
-      alert("You win!");
-      $("#alfheimNextDiv").show();
-    };
+    console.log("answer array: " + answerArray);
     if (print === true) {
-
-      //find each instance of that letter in wordArray
-      //get thier indexes
-      //
+      game.displayResult(letter, answerArray, answer, board); // this is broken
     } else {
       $(".displayLivesLeft").append("Guesses Left: " + game.guessesLeft);
     }
-  })
+    //check for win/loss:
+    var final = game.checkForEndGame(answer);
+    if (final === true && game.guessesLeft === 0) {
+      alert("You lost the game! Lose a life and continue");
+      $("#alfheimNextDiv").show();
+    } else if (final === true) {
+      alert("You win!");
+      $("#alfheimNextDiv").show();
+    };
+  });
 
 
-})
+});
